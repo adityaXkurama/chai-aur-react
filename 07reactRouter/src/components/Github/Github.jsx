59b485c56184db3e21efdee7fcb1ffd393aadfcf
adityaXkurama <raybandarkaraditya@gmail.com>
2024-08-08 17:53:11 +0530

    project to learn the react router dom concept

---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+export default function Github() {
+    const data=useLoaderData()
+     // const [data,setData]=useState([])
+    // useEffect(()=>{
+    //     fetch('https://api.github.com/users/adityaXkurama')
+    //     .then(Response=>Response.json())
+    //     .then(data=>{
+    //         console.log(data);
+    //         setData(data)
+    //     })
+    // },[])
+    // useEffect(()=>{
+    //     fetch('https://api.github.com/users/hiteshchoudhary')
+    //     .then(Response=>Response.json())
+    //     .then(data=>{
+    //         console.log(data);
+    //         setData(data)
+    //     })
+    // },[])
+  return (
+    <div className='bg-gray-600 text-white p-4 test-center m-4 text-3xl'>
+        Github Followers: {data.followers}
+        <img src={data.avatar_url} alt="Git picture" />
+    </div>
+  )
+}
+
+export const githubInfoLoader =async()=>{
+    const Response=await fetch('https://api.github.com/users/hiteshchoudhary')
+    return Response.json()
+}
---
+++ 07reactRouter/src/components/User/User.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+export default function User() {
+    const {userid}=useParams()
+  return (
+    <div>
+      User :{userid}
+    </div>
+  )
+}
